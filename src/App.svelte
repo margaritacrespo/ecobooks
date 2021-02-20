45be83b0814a50533e66258661fba961f4f25cad
Luis Crespo <lcrespom@gmail.com>
2021-02-20 18:56:21 +0100

    Initial version

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import NavMenu from './NavMenu.svelte'
+
+  let menuOptions = {
+    donate: 'Donar Libros',
+    reserve: 'Reservar Libros'
+  }
+</script>
+
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#/">Aula Ecobooks</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse ms-4" id="navbarNav">
+        <NavMenu {menuOptions}/>
+      </div>
+    </div>
+  </nav>
+</header>
+<main>
+  <!--
+    For router support: https://github.com/ItalyPaleAle/svelte-spa-router
+    Example: https://svelte.dev/repl/11e51e1ce65449c0b0db1af0906e206a?version=3.4.4
+  -->
+	<h1 class="m-4">ToDo</h1>
+</main>
+
+<style>
+</style>
---
+++ src/NavMenu.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let menuOptions
+
+  console.log(menuOptions)
+</script>
+
+<ul class="navbar-nav">
+  <li class="nav-item">
+    <a class="nav-link" aria-current="page" href="#/">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#/">Features</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#/">Pricing</a>
+  </li>
+</ul>
