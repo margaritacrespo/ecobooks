4207b75ea6b423664ddd22c77b2087198ffb43f6
Luis Crespo <lcrespom@gmail.com>
2021-02-20 19:51:54 +0100

    Iterate over menu options

--- src/NavMenu.svelte
+++ src/NavMenu.svelte
@@ -1,13 +1,26 @@
 <script>
   export let menuOptions
+  let selectedRoute = null
 
-  console.log(menuOptions)
+  function getSelected(route) {
+    return route == selectedRoute ? 'selected' : ''
+  }
+
+  function routeClicked(route) {
+    selectedRoute = route
+  }
 </script>
 
 <ul class="navbar-nav">
   {#each menuOptions as { route, label }}
     <li class="nav-item">
-      <a class="nav-link" href="#/{route}/">{label}</a>
+      <a
+        class="nav-link {getSelected(route)}"
+        href="#/{route}/"
+        on:click={() => routeClicked(route)}
+      >
+        {label}
+      </a>
     </li>
   {/each}
 </ul>
