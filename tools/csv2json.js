e4334d7bb317202ef771eb810c299aed72843e17
Luis Crespo <lcrespom@gmail.com>
2021-07-04 16:44:42 +0200

    Added json

---
+++ tools/csv2json.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+function main() {
+    const tsv = fs.readFileSync(process.argv[2], 'utf-8')
+    let books = []
+    let lines = tsv.split('\n').map(l => l.trim())
+    for (let line of lines) {
+        let cells = line.split('\t')
+        books.push({
+            grade: cells[0],
+            subject: cells[1],
+            title: cells[2],
+            isbn: cells[3],
+            amount: +cells[4]
+        })
+    }
+    books = books.filter(b => b.amount > 0)
+    console.log(JSON.stringify(books, null, 2))
+}
+
+main()
